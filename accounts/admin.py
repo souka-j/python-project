4223222d4eccdb8433d544b0274b2641d672f3eb
Soukaina <soukainajabir88@gmail.com>
2026-01-17 20:05:14 +0100

    update project

--- accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-# Pour gérer User dans l'admin Django
-admin.site.register(User)
--- accounts/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import User
-from .serializers import UserSerializer
-
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()       # Tous les utilisateurs
-    serializer_class = UserSerializer   # Sérializer pour transformer en JSON
